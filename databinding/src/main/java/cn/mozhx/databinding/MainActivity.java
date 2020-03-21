b2033669e01730a3b6668885cde96c37a7147640
Harry <mozhx020@163.com>
2020-03-21 19:33:07 +0800

    databinding

--- databinding/src/main/java/cn/mozhx/databinding/MainActivity.java
+++ databinding/src/main/java/cn/mozhx/databinding/MainActivity.java
@@ -3,9 +3,8 @@ package cn.mozhx.databinding;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Toast;
 
 import cn.mozhx.databinding.databinding.ActivityMainBinding;
 import cn.mozhx.databinding.pojo.User;
@@ -22,19 +21,30 @@ public class MainActivity extends AppCompatActivity {
         //[pkg].databinding.* 不会自动导入?
         final ActivityMainBinding dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        //初始化data 还没有初始化时dataBinding的data还是空的
+        User user = new User();
+        user.setLastName("init");
+        dataBinding.setUser(user);
+
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 User user = new User("Mok", "Harry");
-                EditText editText = (EditText) findViewById(R.id.et_msg);
-                String s = editText.getText().toString().trim();
-                if(TextUtils.isEmpty(s)){
-                    s = editText.getHint().toString().trim();
-                }
-                user.setLastName(s);
+                user.setLastName("Harry" + (++tmp));
                 dataBinding.setUser(user);
             }
         });
+
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User user = dataBinding.getUser();
+                Toast.makeText(MainActivity.this, user.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+    int tmp;
 
 }
--- databinding/src/main/java/cn/mozhx/databinding/pojo/User.java
+++ databinding/src/main/java/cn/mozhx/databinding/pojo/User.java
@@ -1,11 +1,18 @@
 package cn.mozhx.databinding.pojo;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * @author @Harry
  * @date 2018-12-29
  */
-public class User {
+public class User extends BaseObservable {
+
     private String firstName;
+
+    //需要使用对属性或属性的getter使用注解@Bindable 否则BR不会生成对应属性
+    @Bindable
     private String lastName;
 
     public User() {
@@ -30,5 +37,14 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+        notifyPropertyChanged(cn.mozhx.databinding.BR.lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 }
